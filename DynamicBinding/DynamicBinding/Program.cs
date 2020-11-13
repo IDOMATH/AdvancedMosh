0dfc20921c8121aa9f924e0c0b0c2e9b473a2306
IDOMATH <btdothemath@gmail.com>
2020-11-13 13:19:09 -0600

    Dynamics Lecture

---
+++ DynamicBinding/DynamicBinding/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DynamicBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object obj = "Blane";
+
+            //var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            //methodInfo.Invoke(null, null);
+
+            // We know this object will have the optimize method at run time so we can use dynamic typing keyword
+            //dynamic excelObject = "blane";
+            //excelObject.Optimize();
+
+            dynamic a = 10;
+            a = "hi";
+        }
+    }
+}
