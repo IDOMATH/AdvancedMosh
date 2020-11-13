16228c07166ee40fd8c8a34d7341cf4f7bbc028d
IDOMATH <btdothemath@gmail.com>
2020-11-13 15:40:25 -0600

    Async/Await lecture

---
+++ Async/Async/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Async
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var url = @"https://docs.microsoft.com/en-us/";
+            //DownloadHtml(url);
+            //DownloadHtmlAsync(url);
+
+            //var html = GetHtml(url);
+            var html = await GetHtmlAsync(url);
+            MessageBox.Show(html.Substring(0,10));
+        }
+        public async Task<string> GetHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+            return await webClient.DownloadStringTaskAsync(url);
+        }
+        public string GetHtml(string url)
+        {
+            var webClient = new WebClient();
+            return webClient.DownloadString(url);
+        }
+        public async Task DownloadHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+            var html = await webClient.DownloadStringTaskAsync(url);
+
+            using (var streamWriter = new StreamWriter(@"c:\logs\result.html"))
+            {
+                await streamWriter.WriteAsync(html);
+            }
+        }
+        public void DownloadHtml(string url)
+        {
+            var webClient = new WebClient();
+            var html = webClient.DownloadString(url);
+
+            using (var streamWriter = new StreamWriter(@"c:\logs\result.html"))
+            {
+                streamWriter.Write(html);
+            }
+        }
+    }
+}
