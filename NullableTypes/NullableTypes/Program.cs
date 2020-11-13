239bd57533a64ab853306202536191c99c300eb5
IDOMATH <btdothemath@gmail.com>
2020-11-13 13:07:35 -0600

    Nullable Types

---
+++ NullableTypes/NullableTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nullable<DateTime> date = null;
+            DateTime? date = null;
+            Console.WriteLine("getvalue or default" + date.GetValueOrDefault());
+            Console.WriteLine("Has value: " + date.HasValue);
+            Console.WriteLine("Value: " + date.Value);
+
+            DateTime date2;
+            DateTime date3 = date ?? DateTime.Today;
+
+
+            if (date != null) date2 = date.GetValueOrDefault();
+            else date2 = DateTime.Today;
+
+
+        }
+    }
+}
