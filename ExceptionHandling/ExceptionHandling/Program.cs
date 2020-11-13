4d65d7d67d731400dafee2884bf925944da643a0
IDOMATH <btdothemath@gmail.com>
2020-11-13 14:46:13 -0600

    ExceptionHandling lecture

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    var calculator = new Calculator();
+            //    var result = calculator.Divide(5, 0);
+            //}
+            //catch (DivideByZeroException)
+            //{
+            //    Console.WriteLine("cannot divide by zero");
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("sorry, an error occurred");
+            //}
+
+            //StreamReader streamReader = null;
+            //try
+            //{
+            //    streamReader = new StreamReader(@"C:\logs\log.txt");
+            //    var content = streamReader.ReadToEnd();
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("an unexpected error occurred.");
+            //}
+            //finally
+            //{
+            //    if (streamReader != null)
+            //        streamReader.Dispose();
+            //}
+
+            //try
+            //{
+            //    using (var streamReader = new StreamReader(@"c:\logs\log.txt"))
+            //    {
+            //        var content = streamReader.ReadToEnd();
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("an error occurred");
+            //}
+
+            try
+            {
+                var api = new YouTubeApi();
+                var videos = api.GetVideos("IDOMATH");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/YouTubeApi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+    public class YouTubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                //Stuff
+                throw new Exception("some low-level error");
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                throw new YouTubeException("Something went wrong retrieving videos", ex);
+            }
+
+            return new List<Video>();
+        }
+    }
+}
