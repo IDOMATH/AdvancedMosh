f559b128151812e170668f385b33b898b736c70a
IDOMATH <btdothemath@gmail.com>
2020-11-12 21:17:54 -0600

    LambdaExpressions lecture

---
+++ LambdaExpressions/LambdaExpressions/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LambdaExpressions
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() { Title = "Title1", Price = 5 },
+                new Book() { Title = "Title2", Price = 7 },
+                new Book() { Title = "TIlte3", Price = 17 }
+            };
+        }
+    }
+}
---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Func<int, int> square = number => number * number;
+
+            //const int factor = 5;
+            //Func<int, int> multiplier = n => n * factor;
+
+            //Console.WriteLine(multiplier(10));
+            //Console.WriteLine(square(5));
+            //Console.WriteLine(Square(5));
+
+            var books = new BookRepository().GetBooks();
+            var cheapBooks = books.FindAll(book => book.Price < 10);
+
+            //    var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+        }
+        static bool IsCheaperThan10Dollars(Book book)
+        {
+            return book.Price < 10;
+        }
+        static int Square(int number)
+        {
+            return number * number;
+        }
+    }
+}
